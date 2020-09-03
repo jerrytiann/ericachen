0296cd9932cccf3d7414c7a61bc091d1b9bd495c
Jerry <jerrytiann@gmail.com>
2020-09-03 11:42:58 +1000

    edited navbar

--- Version2/scripts/app.js
+++ Version2/scripts/app.js
@@ -52,61 +52,64 @@ function init() {
 
 }
 
-function navAnimation() {
 
-    const hamburger = document.querySelector(".menu");
-    const hamburgerLines = document.querySelectorAll(".menu line");
-    const navOpen = document.querySelector(".nav-open");
-    const contact = document.querySelector(".contact");
-    const social = document.querySelector(".social");
-    const logo = document.querySelector(".logo");
+init();
 
-    const tl = new TimelineMax({
-        paused: true,
-        reversed: true
-    });
+// function navAnimation() {
 
-    tl.to(navOpen, 0.5, {
-            y: 0
-        })
-        .fromTo(contact, 0.5, {
-            opacity: 0,
-            y: 10
-        }, {
-            opacity: 1,
-            y: 0
-        }, "-=0.1")
-        .fromTo(social, 0.5, {
-            opacity: 0,
-            y: 10
-        }, {
-            opacity: 1,
-            y: 0
-        }, "-=0.5")
-        .fromTo(logo, 0.2, {
-            color: "white"
-        }, {
-            color: "black"
-        }, "-=1")
-        .fromTo(
-            hamburgerLines,
-            0.2, {
-                stroke: "white"
-            }, {
-                stroke: "black"
-            },
-            "-=1"
-        );
+//     const hamburger = document.querySelector(".menu");
+//     const hamburgerLines = document.querySelectorAll(".menu line");
+//     const navOpen = document.querySelector(".nav-open");
+//     const contact = document.querySelector(".contact");
+//     const social = document.querySelector(".social");
+//     const logo = document.querySelector(".logo");
 
-    hamburger.addEventListener("click", () => {
-        tl.reversed() ? tl.play() : tl.reverse();
-    });
+//     const tl = new TimelineMax({
+//         paused: true,
+//         reversed: true
+//     });
 
-}
+//     tl.to(navOpen, 0.5, {
+//             y: 0
+//         })
+//         .fromTo(contact, 0.5, {
+//             opacity: 0,
+//             y: 10
+//         }, {
+//             opacity: 1,
+//             y: 0
+//         }, "-=0.1")
+//         .fromTo(social, 0.5, {
+//             opacity: 0,
+//             y: 10
+//         }, {
+//             opacity: 1,
+//             y: 0
+//         }, "-=0.5")
+//         .fromTo(logo, 0.2, {
+//             color: "white"
+//         }, {
+//             color: "black"
+//         }, "-=1")
+//         .fromTo(
+//             hamburgerLines,
+//             0.2, {
+//                 stroke: "white"
+//             }, {
+//                 stroke: "black"
+//             },
+//             "-=1"
+//         );
 
-const app = () => {
-    navAnimation();
-    init();
-}
+//     hamburger.addEventListener("click", () => {
+//         tl.reversed() ? tl.play() : tl.reverse();
+//     });
 
-app();
+// }
+
+// const app = () => {
+//     navAnimation();
+//     init();
+// }
+
+// app();
