909d9d612963292da9a33558003d62311dc9d312
Jerry <jerrytiann@gmail.com>
2020-09-04 12:02:56 +1000

    formatted js

--- Version2/scripts/app.js
+++ Version2/scripts/app.js
@@ -1,56 +1,66 @@
 // window.alert("hellow")
 
 function init() {
-
     const hero = document.querySelector(".hero");
-    const landing = document.querySelector('.landing-bg');
-    const logo = document.querySelector('.logo');
-    const hamburger = document.querySelector('.menu');
-    const headline = document.querySelector('.headline');
+    const landing = document.querySelector(".landing-bg");
+    const logo = document.querySelector(".logo");
+    const hamburger = document.querySelector(".menu");
+    const headline = document.querySelector(".headline");
 
     const tl = new TimelineMax();
 
-    tl.fromTo(hero, 1, {
-            height: "0%"
-        }, {
-            height: "80%",
-            ease: Power2.easeInOut
-        })
-        .fromTo(hero, 1, {
-            width: "100%"
-        }, {
-            width: "90%"
-        })
-        .fromTo(landing, 1, {
-            x: "-100%"
-        }, {
-            x: "0%",
-            ease: Power2.easeInOut
-        }, "-=1.85")
-        .fromTo(logo, 1, {
-            opacity: 0,
-            x: -30
-        }, {
-            opacity: 1,
-            x: 0
-        }, "-=0.5")
-        .fromTo(hamburger, 1, {
-            opacity: 0,
-            x: -30
-        }, {
-            opacity: 1,
-            x: 0
-        }, "-=0.5");
-
-
-
+    tl.fromTo(
+            hero,
+            1, {
+                height: "0%",
+            }, {
+                height: "80%",
+                ease: Power2.easeInOut,
+            }
+        )
+        .fromTo(
+            hero,
+            1, {
+                width: "100%",
+            }, {
+                width: "90%",
+            }
+        )
+        .fromTo(
+            landing,
+            1, {
+                x: "-100%",
+            }, {
+                x: "0%",
+                ease: Power2.easeInOut,
+            },
+            "-=1.85"
+        )
+        .fromTo(
+            logo,
+            1, {
+                opacity: 0,
+                x: -30,
+            }, {
+                opacity: 1,
+                x: 0,
+            },
+            "-=0.5"
+        )
+        .fromTo(
+            hamburger,
+            1, {
+                opacity: 0,
+                x: -30,
+            }, {
+                opacity: 1,
+                x: 0,
+            },
+            "-=0.5"
+        );
 }
 
-
-
-
 function navAnimation() {
-
     const hamburger = document.querySelector(".menu");
     const hamburgerLines = document.querySelectorAll(".menu line");
     const navOpen = document.querySelector(".nav-open");
@@ -60,37 +70,49 @@ function navAnimation() {
 
     const tl = new TimelineMax({
         paused: true,
-        reversed: true
+        reversed: true,
     });
 
     tl.to(navOpen, 0.5, {
-            y: 0
+            y: 0,
         })
-        .fromTo(contact, 0.5, {
-            opacity: 0,
-            y: 10
-        }, {
-            opacity: 1,
-            y: 0
-        }, "-=0.1")
-        .fromTo(social, 0.5, {
-            opacity: 0,
-            y: 10
-        }, {
-            opacity: 1,
-            y: 0
-        }, "-=0.5")
-        .fromTo(logo, 0.2, {
-            color: "white"
-        }, {
-            color: "black"
-        }, "-=1")
+        .fromTo(
+            contact,
+            0.5, {
+                opacity: 0,
+                y: 10,
+            }, {
+                opacity: 1,
+                y: 0,
+            },
+            "-=0.1"
+        )
+        .fromTo(
+            social,
+            0.5, {
+                opacity: 0,
+                y: 10,
+            }, {
+                opacity: 1,
+                y: 0,
+            },
+            "-=0.5"
+        )
+        .fromTo(
+            logo,
+            0.2, {
+                color: "white",
+            }, {
+                color: "black",
+            },
+            "-=1"
+        )
         .fromTo(
             hamburgerLines,
             0.2, {
-                stroke: "white"
+                stroke: "white",
             }, {
-                stroke: "black"
+                stroke: "black",
             },
             "-=1"
         );
@@ -98,13 +120,11 @@ function navAnimation() {
     hamburger.addEventListener("click", () => {
         tl.reversed() ? tl.play() : tl.reverse();
     });
-
 }
-
 
 const app = () => {
     init();
     navAnimation();
-}
+};
 
 app();
